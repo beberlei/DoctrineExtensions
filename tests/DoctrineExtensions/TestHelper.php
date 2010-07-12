6c61cf6116dbdcf32ae6f9944ce61365fc1d5de6
Benjamin Eberlei <kontakt@beberlei.de>
2010-07-12 13:19:41 +0200

    Fix TestHelper to run with new Doctrine 2 architecture

--- tests/DoctrineExtensions/TestHelper.php
+++ tests/DoctrineExtensions/TestHelper.php
@@ -4,13 +4,19 @@ if (!isset($GLOBALS['doctrine2-path'])) {
     throw new InvalidArgumentException('Global variable "doctrine2-path" has to be set in phpunit.xml');
 }
 
-$loaderfile = $GLOBALS['doctrine2-path']."/Doctrine/Common/ClassLoader.php";
+$loaderfile = $GLOBALS['doctrine2-path']."/vendor/doctrine-common/lib/Doctrine/Common/ClassLoader.php";
 if (!file_exists($loaderfile)) {
     throw new InvalidArgumentException('Could not include Doctrine\Common\ClassLoader from "doctrine2-path".');
 }
 require_once($loaderfile);
 
-$loader = new Doctrine\Common\ClassLoader("Doctrine", $GLOBALS['doctrine2-path']);
+$loader = new Doctrine\Common\ClassLoader("Doctrine\Common", $GLOBALS['doctrine2-path'] . "/vendor/doctrine-common/lib/");
+$loader->register();
+
+$loader = new Doctrine\Common\ClassLoader("Doctrine\DBAL", $GLOBALS['doctrine2-path'] . "/vendor/doctrine-dbal/lib/");
+$loader->register();
+
+$loader = new Doctrine\Common\ClassLoader("Doctrine\ORM", $GLOBALS['doctrine2-path']);
 $loader->register();
 
 $loader = new Doctrine\Common\ClassLoader("DoctrineExtensions", __DIR__."/../../lib");
