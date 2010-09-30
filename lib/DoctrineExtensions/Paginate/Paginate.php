90aec129cbdd93023a93edb17960520d110e141d
Guilherme Blanco <guilhermeblanco@gmail.com>
2010-09-29 23:25:25 -0300

    Implemented parameters export on Paginator.

--- lib/DoctrineExtensions/Paginate/Paginate.php
+++ lib/DoctrineExtensions/Paginate/Paginate.php
@@ -7,6 +7,23 @@ class Paginate
 {
     /**
      * @param Query $query
+     * @return Query
+     */
+    static protected function cloneQuery(Query $query)
+    {
+        /* @var $countQuery Query */
+        $countQuery = clone $query;
+        $params = $query->getParameters();
+
+        foreach ($params as $key => $param) {
+            $countQuery->setParameter($key, $param);
+        }
+
+        return $countQuery;
+    } 
+
+    /**
+     * @param Query $query
      * @return int
      */
     static public function count(Query $query)
@@ -43,7 +60,7 @@ class Paginate
     static public function createCountQuery(Query $query)
     {
         /* @var $countQuery Query */
-        $countQuery = clone $query;
+        $countQuery = self::cloneQuery($query);
 
         $countQuery->setHint(Query::HINT_CUSTOM_TREE_WALKERS, array('DoctrineExtensions\Paginate\CountWalker'));
         $countQuery->setFirstResult(null)->setMaxResults(null);
@@ -59,7 +76,7 @@ class Paginate
      */
     static public function createLimitSubQuery(Query $query, $offset, $itemCountPerPage)
     {
-        $subQuery = clone $query;
+        $subQuery = self::cloneQuery($query);
         $subQuery->setHint(Query::HINT_CUSTOM_TREE_WALKERS, array('DoctrineExtensions\Paginate\LimitSubqueryWalker'))
             ->setFirstResult($offset)
             ->setMaxResults($itemCountPerPage);
@@ -74,15 +91,18 @@ class Paginate
      */
     static public function createWhereInQuery(Query $query, array $ids, $namespace = 'pgid')
     {
-        $whereInQuery = clone $query;
+        $whereInQuery = self::cloneQuery($query);
         $whereInQuery->setHint(Query::HINT_CUSTOM_TREE_WALKERS, array('DoctrineExtensions\Paginate\WhereInWalker'));
         $whereInQuery->setHint('id.count', count($ids));
         $whereInQuery->setHint('pg.ns', $namespace);
         $whereInQuery->setFirstResult(null)->setMaxResults(null);
+
         foreach ($ids as $i => $id) {
-            $i = $i+1;
+            $i = $i + 1;
+
             $whereInQuery->setParameter("{$namespace}_{$i}", $id);
         }
+
         return $whereInQuery;
     }
 }
