201f495cd57494148b1ed8f4acca9231a2515c1e
Chris Woodford <chris.woodford@gmail.com>
2011-04-17 13:21:17 -0400

    added asin test case

--- tests/DoctrineExtensions/Query/MysqlUdfTest.php
+++ tests/DoctrineExtensions/Query/MysqlUdfTest.php
@@ -12,11 +12,11 @@ class MysqlUdfTest extends \PHPUnit_Framework_TestCase
         $config = new \Doctrine\ORM\Configuration();
         $config->setMetadataCacheImpl(new \Doctrine\Common\Cache\ArrayCache());
         $config->setQueryCacheImpl(new \Doctrine\Common\Cache\ArrayCache());
-        $config->setProxyDir('/Users/User/workspace/Doctrine Extensions/tests/Entities/Proxies');
-        $config->setProxyNamespace('Entities\Proxies');
+        $config->setProxyDir($GLOBALS['doctrine2-proxies-path']);
+        $config->setProxyNamespace($GLOBALS['doctrine2-proxies-namespace']);
         $config->setAutoGenerateProxyClasses(true);
 
-        $driver = $config->newDefaultAnnotationDriver('/Users/User/workspace/Doctrine Extensions/tests/Entities');
+        $driver = $config->newDefaultAnnotationDriver($GLOBALS['doctrine2-entities-path']);
         $config->setMetadataDriverImpl($driver);
 
         $conn = array(
@@ -26,7 +26,16 @@ class MysqlUdfTest extends \PHPUnit_Framework_TestCase
 
         $config->addCustomNumericFunction('DATEDIFF', 'DoctrineExtensions\Query\Mysql\DateDiff');
         $config->addCustomDatetimeFunction('DATE_ADD', 'DoctrineExtensions\Query\Mysql\DateAdd');
+
         $config->addCustomNumericFunction('SIN', 'DoctrineExtensions\Query\Mysql\Sin');
+        $config->addCustomNumericFunction('ASIN', 'DoctrineExtensions\Query\Mysql\Asin');
+        $config->addCustomNumericFunction('COS', 'DoctrineExtensions\Query\Mysql\Cos');
+        $config->addCustomNumericFunction('ACOS', 'DoctrineExtensions\Query\Mysql\Acos');
+        $config->addCustomNumericFunction('COT', 'DoctrineExtensions\Query\Mysql\Cot');
+        $config->addCustomNumericFunction('TAN', 'DoctrineExtensions\Query\Mysql\Tan');
+
+        $config->addCustomNumericFunction('DEGREES', 'DoctrineExtensions\Query\Mysql\Degrees');
+        $config->addCustomNumericFunction('RADIANS', 'DoctrineExtensions\Query\Mysql\Radians');
 
         $this->entityManager = \Doctrine\ORM\EntityManager::create($conn, $config);
 
@@ -76,6 +85,23 @@ class MysqlUdfTest extends \PHPUnit_Framework_TestCase
         $q = $this->entityManager->createQuery($dql);
 
         $sql = "SELECT SIN(b0_.latitude) AS sclr0 FROM BlogPost b0_";
+        $this->assertEquals($sql, $q->getSql());
+
+    }
+
+    public function testAsin()
+    {
+
+        $dql = "SELECT p FROM Entities\BlogPost p WHERE ASIN(p.latitude) = 1";
+        $q = $this->entityManager->createQuery($dql);
+
+        $sql = "SELECT b0_.id AS id0, b0_.created AS created1, b0_.longitude AS longitude2, b0_.latitude AS latitude3 FROM BlogPost b0_ WHERE ASIN(b0_.latitude) = 1";
+    	$this->assertEquals($sql, $q->getSql());
+
+        $dql = "SELECT ASIN(p.latitude) FROM Entities\BlogPost p";
+        $q = $this->entityManager->createQuery($dql);
+
+        $sql = "SELECT ASIN(b0_.latitude) AS sclr0 FROM BlogPost b0_";
         $this->assertEquals($sql, $q->getSql());
 
     }
