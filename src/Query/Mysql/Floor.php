36511469fa5510ef7bb19ed3a7b575a564eab3a4
Jáchym Toušek <enumag@gmail.com>
2015-09-01 11:58:49 +0200

    Add MySQL CEIL() and FLOOR() functions

---
+++ src/Query/Mysql/Floor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace DoctrineExtensions\Query\Mysql;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode,
+    Doctrine\ORM\Query\Lexer;
+
+class Floor extends FunctionNode
+{
+    private $arithmeticExpression;
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return 'FLOOR(' . $sqlWalker->walkSimpleArithmeticExpression($this->arithmeticExpression) . ')';
+    }
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->arithmeticExpression = $parser->SimpleArithmeticExpression();
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
