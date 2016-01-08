6c427b4e36c478cad805d45537d4e85289911a01
djbfmeel <vanmeeldennis@msn.com>
2016-01-08 13:03:07 +0100

    Added expression to RAND function

--- src/Query/Mysql/Rand.php
+++ src/Query/Mysql/Rand.php
@@ -3,19 +3,28 @@
 namespace DoctrineExtensions\Query\Mysql;
 
 use Doctrine\ORM\Query\AST\Functions\FunctionNode,
-    Doctrine\ORM\Query\Lexer;
+    Doctrine\ORM\Query\Lexer,
+    Doctrine\ORM\Query\AST\Node;
 
 class Rand extends FunctionNode
 {
+    /**
+     * @var Node
+     */
+    private $expression = null;
+
     public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
     {
-        return 'RAND()';
+        return 'RAND(' . $this->expression->dispatch($sqlWalker) . ')';
     }
 
     public function parse(\Doctrine\ORM\Query\Parser $parser)
     {
         $parser->match(Lexer::T_IDENTIFIER);
         $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->expression = $parser->ArithmeticPrimary();
+
         $parser->match(Lexer::T_CLOSE_PARENTHESIS);
     }
 }
