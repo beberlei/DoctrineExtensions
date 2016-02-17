840a5166f978bfccf3a98f23d2ca2ddf838138f9
Ryszard <rustedwolf@gmail.com>
2016-02-17 11:49:55 +0100

    Added support for MySQL TIMEDIFF function

---
+++ src/Query/Mysql/TimeDiff.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace DoctrineExtensions\Query\Mysql;
+
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+
+class TimeDiff extends FunctionNode
+{
+    public $firstDateExpression = null;
+    public $secondDateExpression = null;
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->firstDateExpression = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_COMMA);
+        $this->secondDateExpression = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return sprintf('TIMEDIFF(%s, %s)',
+            $this->firstDateExpression->dispatch($sqlWalker),
+            $this->secondDateExpression->dispatch($sqlWalker)
+        );
+    }
+}
