6090c26b98f55f99602b9178472f5b8c2f17ea6a
FlashBlack <flabla.com@gmail.com>
2016-04-25 16:48:22 +0300

    Change alias name

--- tests/Query/Postgresql/CountFilterTest.php
+++ tests/Query/Postgresql/CountFilterTest.php
@@ -9,7 +9,7 @@ class CountFilterTest extends \DoctrineExtensions\Tests\Query\PostgresqlTestCase
         $dql = "SELECT COUNT(b) FILTER( WHERE b.id = :id) FROM DoctrineExtensions\Tests\Entities\Blank b";
         $q = $this->entityManager->createQuery($dql);
         $q->setParameter('id', 1);
-        $sql = 'SELECT COUNT(d0_.id) FILTER( WHERE d0_.id = ?) FROM Blank d0_';
+        $sql = 'SELECT COUNT(b0_.id) FILTER( WHERE b0_.id = ?) FROM Blank b0_';
         $this->assertEquals($sql, $q->getSql());
     }
 }
