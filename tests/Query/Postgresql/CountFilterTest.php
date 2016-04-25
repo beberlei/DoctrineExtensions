64f9506be99fbae225943287f150107554c7d718
FlashBlack <flabla.com@gmail.com>
2016-04-26 02:18:25 +0300

    Solve CountFilterTest unittest

--- tests/Query/Postgresql/CountFilterTest.php
+++ tests/Query/Postgresql/CountFilterTest.php
@@ -6,7 +6,7 @@ class CountFilterTest extends \DoctrineExtensions\Tests\Query\PostgresqlTestCase
 {
     public function testCountFilter()
     {
-        $dql = "SELECT COUNT(b) FILTER( WHERE b.id = :id) FROM DoctrineExtensions\Tests\Entities\Blank b";
+        $dql = "SELECT COUNT_FILTER(b, WHERE b.id = :id) FROM DoctrineExtensions\Tests\Entities\Blank b";
         $q = $this->entityManager->createQuery($dql);
         $q->setParameter('id', 1);
         $sql = 'SELECT COUNT(b0_.id) FILTER( WHERE b0_.id = ?) FROM Blank b0_';
