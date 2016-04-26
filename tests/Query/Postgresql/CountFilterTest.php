31e8330b94e4637f38a4d0527db789d5b15e3d89
FlashBlack <flabla.com@gmail.com>
2016-04-26 11:02:52 +0300

    Fix support doctrine 2.5 field alias name

--- tests/Query/Postgresql/CountFilterTest.php
+++ tests/Query/Postgresql/CountFilterTest.php
@@ -9,7 +9,7 @@ class CountFilterTest extends \DoctrineExtensions\Tests\Query\PostgresqlTestCase
         $dql = "SELECT COUNT_FILTER(b, WHERE b.id = :id) FROM DoctrineExtensions\Tests\Entities\Blank b";
         $q = $this->entityManager->createQuery($dql);
         $q->setParameter('id', 1);
-        $sql = 'SELECT COUNT(b0_.id) FILTER( WHERE b0_.id = ?) AS sclr0 FROM Blank b0_';
+        $sql = 'SELECT COUNT(b0_.id) FILTER( WHERE b0_.id = ?) AS sclr_0 FROM Blank b0_';
         $this->assertEquals($sql, $q->getSql());
     }
 }
