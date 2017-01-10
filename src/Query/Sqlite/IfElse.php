a9b4f0fdd1cfa4be5c81ff202cd1c8d9d9d303ee
Bubnov <mihail.bubnov@kelnik.ru>
2017-01-10 17:57:15 +0300

    Add Query/Sqlite/IfElse with tests

---
+++ src/Query/Sqlite/IfElse.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace DoctrineExtensions\Query\Sqlite;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+
+/**
+ * @author Mikhail Bubnov <bubnov.mihail@gmail.com>
+ */
+class IfElse extends FunctionNode
+{
+    private $expr = array();
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->expr[] = $parser->ConditionalExpression();
+
+        for ($i = 0; $i < 2; $i++)
+        {
+            $parser->match(Lexer::T_COMMA);
+            $this->expr[] = $parser->ArithmeticExpression();
+        }
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return sprintf('CASE WHEN %s THEN %s ELSE %s END',
+            $sqlWalker->walkConditionalExpression($this->expr[0]),
+            $sqlWalker->walkArithmeticPrimary($this->expr[1]),
+            $sqlWalker->walkArithmeticPrimary($this->expr[2]));
+    }
+}
