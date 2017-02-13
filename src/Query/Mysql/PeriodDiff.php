d11f2c4a69a97460403a11a0a1261aade7447007
Einar Gangsø <mail@einargangso.no>
2017-02-13 15:23:31 +0100

    Initial PERIOD_DIFF, clone from DateDiff

---
+++ src/Query/Mysql/PeriodDiff.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace DoctrineExtensions\Query\Mysql;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode,
+    Doctrine\ORM\Query\Lexer;
+
+class PeriodDiff extends FunctionNode
+{
+    public $firstDateExpression = null;
+    public $secondDateExpression = null;
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->firstDateExpression = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_COMMA);
+        $this->secondDateExpression = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return 'PERIOD_DIFF(' .
+            $sqlWalker->walkArithmeticTerm($this->firstDateExpression) . ', ' .
+            $sqlWalker->walkArithmeticTerm($this->secondDateExpression) .
+        ')';
+    }
+}
