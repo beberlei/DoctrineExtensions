5b8aaf570fa53d287e27339512013350cbf0298c
David Baucum <dbaucum@captiveyes.com>
2017-02-14 01:50:40 -0500

    Add test for column aliases with DateDiff. Fixes #218.

--- tests/Query/Mysql/DateDiffTest.php
+++ tests/Query/Mysql/DateDiffTest.php
@@ -14,4 +14,14 @@ class DateDiffTest extends MysqlTestCase
             $q->getSql()
         );
     }
+
+    public function testDateDiffWithColumnAlias()
+    {
+        $q = $this->entityManager->createQuery("SELECT DATEADD(p.created, 1, 'DAY') as modified_date FROM DoctrineExtensions\Tests\Entities\Date as p WHERE DATEDIFF(CURRENT_TIME(), modified_date) < 7");
+
+        $this->assertEquals(
+            "SELECT DATE_ADD(d0_.created, INTERVAL 1 DAY) AS sclr0 FROM Date d0_ WHERE DATEDIFF(CURRENT_TIME, sclr0) < 7",
+            $q->getSql()
+        );
+    }
 }
