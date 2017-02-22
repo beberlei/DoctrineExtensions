2e198e935e236703ec51d40069940ad0b1650e8f
Einar Gangsø <mail@einargangso.no>
2017-02-22 09:15:48 +0100

    fix use DATEADD with negative number

--- tests/Query/Mysql/DateTest.php
+++ tests/Query/Mysql/DateTest.php
@@ -22,6 +22,15 @@ class DateTest extends \DoctrineExtensions\Tests\Query\MysqlTestCase
         $this->assertEquals($sql, $q->getSql());
     }
 
+    public function testDateAddWithNegative()
+    {
+        $dql = "SELECT p FROM DoctrineExtensions\Tests\Entities\Date p WHERE DATEADD(CURRENT_TIME(), -4, 'MONTH') < 7";
+        $q = $this->entityManager->createQuery($dql);
+        $sql = "SELECT d0_.id AS id_0, d0_.created AS created_1 FROM Date d0_ WHERE DATE_ADD(CURRENT_TIME, INTERVAL -4 MONTH) < 7";
+
+        $this->assertEquals($sql, $q->getSql());
+    }
+
     public function testDateSub()
     {
         $dql = "SELECT p FROM DoctrineExtensions\Tests\Entities\Date p WHERE DATESUB(CURRENT_TIME(), 4, 'MONTH') < 7";
