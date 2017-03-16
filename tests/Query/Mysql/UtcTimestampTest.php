64d7efea61e02d3a9a768e6833ec7ed63e80ad7a
Marius Krämer <marius@marius-kraemer.de>
2017-03-16 10:56:34 +0100

    Add support for UTC_TIMESTAMP on mysql

---
+++ tests/Query/Mysql/UtcTimestampTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace DoctrineExtensions\Tests\Query\Mysql;
+
+class UtcTimestampTest extends \DoctrineExtensions\Tests\Query\MysqlTestCase
+{
+    public function testUtcTimestamp()
+    {
+        $dql = "SELECT d FROM DoctrineExtensions\Tests\Entities\Date d WHERE d.created > utc_timestamp()";
+        $q = $this->entityManager->createQuery($dql);
+
+        $this->assertEquals(
+            'SELECT d0_.id AS id_0, d0_.created AS created_1 FROM Date d0_ WHERE d0_.created > UTC_TIMESTAMP()',
+            $q->getSql()
+        );
+    }
+}
