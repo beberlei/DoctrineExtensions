14a8c4c4c649dca436fc60aeac3eb82fe88933ce
vasn <phpvas@gmail.com>
2017-05-17 17:00:17 +0100

    Mysql function SUBSTRING_INDEX expression issue fixed.

--- src/Query/Mysql/SubstringIndex.php
+++ src/Query/Mysql/SubstringIndex.php
@@ -2,37 +2,47 @@
 
 namespace DoctrineExtensions\Query\Mysql;
 
-use Doctrine\ORM\Query\AST\Functions\FunctionNode,
-    Doctrine\ORM\Query\Lexer;
+use  Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use  Doctrine\ORM\Query\Lexer;
+use  Doctrine\ORM\Query\Parser;
+use  Doctrine\ORM\Query\SqlWalker;
 
 /**
+ * Class SubstringIndex
+ * @package DoctrineExtensions\Query\Mysql
  * @author Vas N <phpvas@gmail.com>
  */
-
 class SubstringIndex extends FunctionNode
 {
-    public $string = null;
-    public $delimiter = null;
-    public $count = null;
+  public $string = null;
+  public $delimiter = null;
+  public $count = null;
 
-    public function parse(\Doctrine\ORM\Query\Parser $parser)
-    {
-        $parser->match(Lexer::T_IDENTIFIER);
-        $parser->match(Lexer::T_OPEN_PARENTHESIS);
-        $this->string = $parser->ArithmeticPrimary();
-        $parser->match(Lexer::T_COMMA);
-        $this->delimiter = $parser->ArithmeticPrimary();
-        $parser->match(Lexer::T_COMMA);
-        $this->count = $parser->ArithmeticPrimary();
-        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
-    }
+  /**
+   * @param \Doctrine\ORM\Query\Parser $parser
+   */
+  public function parse(Parser $parser)
+  {
+    $parser->match(Lexer::T_IDENTIFIER);
+    $parser->match(Lexer::T_OPEN_PARENTHESIS);
+    $this->string = $parser->ArithmeticPrimary();
+    $parser->match(Lexer::T_COMMA);
+    $this->delimiter = $parser->ArithmeticPrimary();
+    $parser->match(Lexer::T_COMMA);
+    $this->count = $parser->ArithmeticExpression();
+    $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+  }
 
-    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
-    {
-        return 'SUBSTRING_INDEX(' .
-            $this->string->dispatch($sqlWalker) . ', ' .
-            $this->delimiter->dispatch($sqlWalker) . ', ' .
-            $this->count->dispatch($sqlWalker) .
-        ')';
-    }
+  /**
+   * @param \Doctrine\ORM\Query\SqlWalker $sqlWalker
+   * @return string
+   */
+  public function getSql(SqlWalker $sqlWalker)
+  {
+    return sprintf("SUBSTRING_INDEX(%s, %s, %s)",
+      $this->string->dispatch($sqlWalker),
+      $this->delimiter->dispatch($sqlWalker),
+      $this->count->dispatch($sqlWalker)
+    );
+  }
 }
