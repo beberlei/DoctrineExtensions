45d366ab4b43ad92fcf3139daebc3f8777230c3e
vasn <phpvas@gmail.com>
2017-05-17 17:33:18 +0100

    Indenting to 4 spaces.

--- src/Query/Mysql/SubstringIndex.php
+++ src/Query/Mysql/SubstringIndex.php
@@ -14,35 +14,35 @@ use  Doctrine\ORM\Query\SqlWalker;
  */
 class SubstringIndex extends FunctionNode
 {
-  public $string = null;
-  public $delimiter = null;
-  public $count = null;
+    public $string = null;
+    public $delimiter = null;
+    public $count = null;
 
-  /**
-   * @param \Doctrine\ORM\Query\Parser $parser
-   */
-  public function parse(Parser $parser)
-  {
-    $parser->match(Lexer::T_IDENTIFIER);
-    $parser->match(Lexer::T_OPEN_PARENTHESIS);
-    $this->string = $parser->ArithmeticPrimary();
-    $parser->match(Lexer::T_COMMA);
-    $this->delimiter = $parser->ArithmeticPrimary();
-    $parser->match(Lexer::T_COMMA);
-    $this->count = $parser->ArithmeticExpression();
-    $parser->match(Lexer::T_CLOSE_PARENTHESIS);
-  }
+    /**
+     * @param \Doctrine\ORM\Query\Parser $parser
+     */
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->string = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_COMMA);
+        $this->delimiter = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_COMMA);
+        $this->count = $parser->ArithmeticExpression();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
 
-  /**
-   * @param \Doctrine\ORM\Query\SqlWalker $sqlWalker
-   * @return string
-   */
-  public function getSql(SqlWalker $sqlWalker)
-  {
-    return sprintf("SUBSTRING_INDEX(%s, %s, %s)",
-      $this->string->dispatch($sqlWalker),
-      $this->delimiter->dispatch($sqlWalker),
-      $this->count->dispatch($sqlWalker)
-    );
-  }
+    /**
+     * @param \Doctrine\ORM\Query\SqlWalker $sqlWalker
+     * @return string
+     */
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return sprintf("SUBSTRING_INDEX(%s, %s, %s)",
+          $this->string->dispatch($sqlWalker),
+          $this->delimiter->dispatch($sqlWalker),
+          $this->count->dispatch($sqlWalker)
+        );
+    }
 }
