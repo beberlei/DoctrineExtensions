076a63db707143296d096fd63f582323f8bd028f
Radoslaw Zagorski <r@zagorski.it>
2017-05-23 11:15:25 +0200

    MySQL: ANY_VALUE function added

---
+++ src/Query/Mysql/AnyValue.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace DoctrineExtensions\Query\Mysql;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+
+class AnyValue extends FunctionNode
+{
+    public $value;
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->value = $parser->StringPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return sprintf(
+            'ANY_VALUE(%s)',
+            $this->value->dispatch($sqlWalker)
+        );
+    }
+}
