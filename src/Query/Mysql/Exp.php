edc0ef1bf7b9f84f91fbd1f27560395ce70a3092
Arnaud Bienvenu <abi@abienvenu.net>
2017-10-02 16:05:08 +0200

    Add EXP, LOG and LOG2 for MySQL

---
+++ src/Query/Mysql/Exp.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace DoctrineExtensions\Query\Mysql;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode,
+    Doctrine\ORM\Query\Lexer;
+
+class Exp extends FunctionNode
+{
+    public $arithmeticExpression;
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return 'EXP(' . $sqlWalker->walkSimpleArithmeticExpression(
+                $this->arithmeticExpression
+        ) . ')';
+    }
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->arithmeticExpression = $parser->SimpleArithmeticExpression();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
