028f914795f2329fd546291eff7fc62f9d3f3161
Steve Lacey <steve@stevelacey.net>
2018-05-28 15:49:36 +0800

    Made blank alias consistent

--- tests/Query/Mysql/BitCountTest.php
+++ tests/Query/Mysql/BitCountTest.php
@@ -6,7 +6,7 @@ class BitCountTest extends \DoctrineExtensions\Tests\Query\MysqlTestCase
 {
     public function testBitCount()
     {
-        $q = $this->entityManager->createQuery("SELECT BIT_COUNT(2) from DoctrineExtensions\Tests\Entities\Blank blank");
+        $q = $this->entityManager->createQuery("SELECT BIT_COUNT(2) from DoctrineExtensions\Tests\Entities\Blank b");
 
         $this->assertEquals(
             "SELECT BIT_COUNT(2) AS sclr_0 FROM Blank b0_",
