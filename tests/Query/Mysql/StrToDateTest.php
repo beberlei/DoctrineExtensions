e383a67849baa06499a902f3b76bed0922c2198b
Barry O'Donovan <barry@opensolutions.ie>
2018-07-14 09:46:51 +0100

    Fix test file name so they are not skipped by phpunit

--- tests/Query/Mysql/StrToDateTest.php
+++ tests/Query/Mysql/StrToDateTest.php
@@ -4,7 +4,7 @@ namespace DoctrineExtensions\Tests\Query\Mysql;
 
 use DoctrineExtensions\Tests\Query\MysqlTestCase;
 
-class StrToDate extends MysqlTestCase
+class StrToDateTest extends MysqlTestCase
 {
     public function testStrToDate()
     {
