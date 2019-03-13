9b85a39c2c804820700fc1bd22a71dfc6cbb834e
Chinmay Shah <chinmay.d.shah@gmail.com>
2019-03-13 14:53:25 +0530

    Add format support in MySQL fromunixtime

--- tests/Query/Mysql/FromUnixTimeTest.php
+++ tests/Query/Mysql/FromUnixTimeTest.php
@@ -12,5 +12,10 @@ class FromUnixTimeTest extends MysqlTestCase
             "SELECT FROM_UNIXTIME(2) from DoctrineExtensions\Tests\Entities\Blank b",
             'SELECT FROM_UNIXTIME(2) AS sclr_0 FROM Blank b0_'
         );
+
+        $this->assertDqlProducesSql(
+            "SELECT FROM_UNIXTIME(2,'%M-%Y') from DoctrineExtensions\Tests\Entities\Blank b",
+            'SELECT FROM_UNIXTIME(2,\'%M-%Y\') AS sclr_0 FROM Blank b0_'
+        );
     }
 }
