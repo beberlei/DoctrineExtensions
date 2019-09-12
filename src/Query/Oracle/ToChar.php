092b3ff67832a87e8948b774d1311d84c36c09e5
Jefferson Vantuir <jeffersonbehling@avato.com.br>
2019-09-12 16:58:22 -0300

    Add nls_language parameter (optional) in To_CHAR

--- src/Query/Oracle/ToChar.php
+++ src/Query/Oracle/ToChar.php
@@ -14,27 +14,47 @@ class ToChar extends FunctionNode
 
     private $fmt;
 
-    private $nls;
+    private $nls = null;
 
     public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
     {
+
+        if ($this->nls) {
+
+            return sprintf(
+                'TO_CHAR(%s, %s, %s)',
+                $sqlWalker->walkArithmeticPrimary($this->datetime),
+                $sqlWalker->walkArithmeticPrimary($this->fmt),
+                $sqlWalker->walkArithmeticPrimary($this->nls)
+            );
+
+        }
+
         return sprintf(
-            'TO_CHAR(%s, %s, %s)',
+            'TO_CHAR(%s, %s)',
             $sqlWalker->walkArithmeticPrimary($this->datetime),
-            $sqlWalker->walkArithmeticPrimary($this->fmt),
-            $sqlWalker->walkArithmeticPrimary($this->nls)
+            $sqlWalker->walkArithmeticPrimary($this->fmt)
         );
+
     }
 
     public function parse(\Doctrine\ORM\Query\Parser $parser)
     {
+
+        $lexer = $parser->getLexer();
+
         $parser->match(Lexer::T_IDENTIFIER);
         $parser->match(Lexer::T_OPEN_PARENTHESIS);
         $this->datetime = $parser->ArithmeticExpression();
         $parser->match(Lexer::T_COMMA);
         $this->fmt = $parser->StringExpression();
-        $parser->match(Lexer::T_COMMA);
-        $this->nls = $parser->StringExpression();
+
+        if ($lexer->isNextToken(Lexer::T_COMMA)) {
+            $parser->match(Lexer::T_COMMA);
+            $this->nls = $parser->StringExpression();
+        }
+
         $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+
     }
 }
