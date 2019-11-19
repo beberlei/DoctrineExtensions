4bb21e46fb61fdba6ddea4d15cf1d82e50a64eb8
Menno Holtkamp <menno.holtkamp@shopforce.nl>
2019-11-19 23:48:53 +0100

    Add "window" functions LAG(), LEAD() and OVER()

---
+++ tests/Query/Mysql/LagTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace DoctrineExtensions\Tests\Query\Mysql;
+
+use DoctrineExtensions\Tests\Query\MysqlTestCase;
+
+class LagTest extends MysqlTestCase
+{
+    public function testLag()
+    {
+        $this->assertDqlProducesSql(
+            'SELECT LAG(COUNT(b.id)) from DoctrineExtensions\Tests\Entities\Blank b',
+            'SELECT LAG(COUNT(b0_.id)) AS sclr_0 FROM Blank b0_'
+        );
+
+        $this->assertDqlProducesSql(
+            'SELECT LAG(COUNT(b.id), 5) from DoctrineExtensions\Tests\Entities\Blank b',
+            'SELECT LAG(COUNT(b0_.id), 5) AS sclr_0 FROM Blank b0_'
+        );
+
+        $this->assertDqlProducesSql(
+            'SELECT LAG(COUNT(b.id), 5, 10) from DoctrineExtensions\Tests\Entities\Blank b',
+            'SELECT LAG(COUNT(b0_.id), 5, 10) AS sclr_0 FROM Blank b0_'
+        );
+
+        $this->assertDqlProducesSql(
+            'SELECT LAG(COUNT(b.id), 5, 5 + 5) from DoctrineExtensions\Tests\Entities\Blank b',
+            'SELECT LAG(COUNT(b0_.id), 5, 5 + 5) AS sclr_0 FROM Blank b0_'
+        );
+
+        $this->assertDqlProducesSql(
+            'SELECT OVER(LAG(COUNT(b.id)), ORDER BY b.id) from DoctrineExtensions\Tests\Entities\Blank b',
+            'SELECT LAG(COUNT(b0_.id)) OVER (ORDER BY b0_.id ASC) AS sclr_0 FROM Blank b0_'
+        );
+
+        $this->assertDqlProducesSql(
+            'SELECT OVER(LAG(COUNT(b.id))) from DoctrineExtensions\Tests\Entities\Blank b',
+            'SELECT LAG(COUNT(b0_.id)) OVER () AS sclr_0 FROM Blank b0_'
+        );
+
+        $this->assertDqlProducesSql(
+            'SELECT OVER(COUNT(b.id) - LAG(COUNT(b.id)), ORDER BY b.id) from DoctrineExtensions\Tests\Entities\Blank b',
+            'SELECT COUNT(b0_.id) - LAG(COUNT(b0_.id)) OVER (ORDER BY b0_.id ASC) AS sclr_0 FROM Blank b0_'
+        );
+    }
+}
