282582435b4bb844da54c697202e539b5c2d2777
Vaskevich Eugeniy <wbrframe@gmail.com>
2019-11-22 16:28:07 +0200

    String_agg DISTINCT and ORDER BY support

--- src/Query/Postgresql/StringAgg.php
+++ src/Query/Postgresql/StringAgg.php
@@ -10,29 +10,49 @@ use Doctrine\ORM\Query\SqlWalker;
 
 /**
  * @author Roberto Júnior <me@robertojunior.net>
+ * @author Vaskevich Eugeniy <wbrframe@gmail.com>
  */
 class StringAgg extends FunctionNode
 {
+    private $orderBy = null;
+
     private $expression = null;
 
     private $delimiter = null;
+
+    private $isDistinct = false;
 
     public function parse(Parser $parser)
     {
         $parser->match(Lexer::T_IDENTIFIER);
         $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $lexer = $parser->getLexer();
+        if ($lexer->isNextToken(Lexer::T_DISTINCT)) {
+            $parser->match(Lexer::T_DISTINCT);
+
+            $this->isDistinct = true;
+        }
+
         $this->expression = $parser->PathExpression(PathExpression::TYPE_STATE_FIELD);
         $parser->match(Lexer::T_COMMA);
         $this->delimiter = $parser->StringPrimary();
+
+        if ($lexer->isNextToken(Lexer::T_ORDER)) {
+            $this->orderBy = $parser->OrderByClause();
+        }
+
         $parser->match(Lexer::T_CLOSE_PARENTHESIS);
     }
 
     public function getSql(SqlWalker $sqlWalker)
     {
-        return sprintf(
-            'string_agg(%s, %s)',
+        return \sprintf(
+            'string_agg(%s%s, %s%s)',
+            ($this->isDistinct ? 'DISTINCT ' : ''),
             $sqlWalker->walkPathExpression($this->expression),
-            $sqlWalker->walkStringPrimary($this->delimiter)
+            $sqlWalker->walkStringPrimary($this->delimiter),
+            ($this->orderBy ? $sqlWalker->walkOrderByClause($this->orderBy) : '')
         );
     }
 }
--- tests/Query/Postgresql/StringAggTest.php
+++ tests/Query/Postgresql/StringAggTest.php
@@ -19,4 +19,56 @@ class StringAggTest extends PostgresqlTestCase
 
         $this->assertEquals($expected, $queryBuilder->getQuery()->getSQL());
     }
+
+    public function testStringAggWithDistinct()
+    {
+        $queryBuilder = new QueryBuilder($this->entityManager);
+        $queryBuilder
+            ->select("string_agg(DISTINCT bpt.latitude, '-')")
+            ->from('DoctrineExtensions\Tests\Entities\BlogPost', 'bpt')
+            ->groupBy('bpt.created');
+
+        $expected = 'SELECT string_agg(DISTINCT b0_.latitude, \'-\') AS sclr_0 FROM BlogPost b0_ GROUP BY b0_.created';
+
+        $this->assertEquals($expected, $queryBuilder->getQuery()->getSQL());
+    }
+
+    public function testStringAggWithOrderByClause()
+    {
+        $queryBuilder = new QueryBuilder($this->entityManager);
+        $queryBuilder
+            ->select("string_agg(bpt.latitude, '-' ORDER BY bpt.latitude)")
+            ->from('DoctrineExtensions\Tests\Entities\BlogPost', 'bpt')
+            ->groupBy('bpt.created');
+
+        $expected = 'SELECT string_agg(b0_.latitude, \'-\' ORDER BY b0_.latitude ASC) AS sclr_0 FROM BlogPost b0_ GROUP BY b0_.created';
+
+        $this->assertEquals($expected, $queryBuilder->getQuery()->getSQL());
+    }
+
+    public function testStringAggWithOrderByClauseAndPredefinedDirectory()
+    {
+        $queryBuilder = new QueryBuilder($this->entityManager);
+        $queryBuilder
+            ->select("string_agg(bpt.latitude, '-' ORDER BY bpt.latitude DESC)")
+            ->from('DoctrineExtensions\Tests\Entities\BlogPost', 'bpt')
+            ->groupBy('bpt.created');
+
+        $expected = 'SELECT string_agg(b0_.latitude, \'-\' ORDER BY b0_.latitude DESC) AS sclr_0 FROM BlogPost b0_ GROUP BY b0_.created';
+
+        $this->assertEquals($expected, $queryBuilder->getQuery()->getSQL());
+    }
+
+    public function testStringAggFullDeclaration()
+    {
+        $queryBuilder = new QueryBuilder($this->entityManager);
+        $queryBuilder
+            ->select("string_agg(DISTINCT bpt.latitude, '-' ORDER BY bpt.latitude DESC)")
+            ->from('DoctrineExtensions\Tests\Entities\BlogPost', 'bpt')
+            ->groupBy('bpt.created');
+
+        $expected = 'SELECT string_agg(DISTINCT b0_.latitude, \'-\' ORDER BY b0_.latitude DESC) AS sclr_0 FROM BlogPost b0_ GROUP BY b0_.created';
+
+        $this->assertEquals($expected, $queryBuilder->getQuery()->getSQL());
+    }
 }
