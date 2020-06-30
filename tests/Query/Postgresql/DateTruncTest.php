5f16b305dda06eda0d2d293c9d2bbd299249a996
Dmitriy V. Mamontov <mamontovdmitriy@users.noreply.github.com>
2020-06-30 06:28:02 +0300

    Add date_trunc function for postgres (#350)

---
+++ tests/Query/Postgresql/DateTruncTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace DoctrineExtensions\Tests\Query\Postgresql;
+
+use Doctrine\ORM\QueryBuilder;
+use DoctrineExtensions\Tests\Query\PostgresqlTestCase;
+
+class DateTruncTest extends PostgresqlTestCase
+{
+    public function testDateTrunc()
+    {
+        $queryBuilder = new QueryBuilder($this->entityManager);
+        $queryBuilder
+            ->select("date_trunc('YEAR', dt.created)")
+            ->from('DoctrineExtensions\Tests\Entities\Date', 'dt');
+
+        $expected = "SELECT DATE_TRUNC('YEAR', d0_.created) AS sclr_0 FROM Date d0_";
+
+        $this->assertEquals($expected, $queryBuilder->getQuery()->getSQL());
+    }
+}
