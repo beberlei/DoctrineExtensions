1adbfc3308ff3baf158e20c8b51ca98c545b1bba
tsadock <tsadock76@hotmail.fr>
2021-01-05 19:25:42 +0100

    Add MySQL WEEKOFYEAR

---
+++ tests/Query/Mysql/WeekOfYearTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DoctrineExtensions\Tests\Query\Mysql;
+
+use DoctrineExtensions\Tests\Query\MysqlTestCase;
+
+class WeekOfYearTest extends MysqlTestCase
+{
+    public function testWeekOfYear()
+    {
+        $this->assertDqlProducesSql(
+            "SELECT WEEKOFYEAR(2) from DoctrineExtensions\Tests\Entities\Blank b",
+            'SELECT WEEKOFYEAR(2) AS sclr_0 FROM Blank b0_'
+        );
+    }
+}
