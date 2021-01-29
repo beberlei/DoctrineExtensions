235b092e42697dcb3b2ebea3a85b79265d148a06
tsadock <tsadock76@hotmail.fr>
2021-01-29 11:03:41 +0100

    Add MySQL WEEKOFYEAR (#362)

---
+++ src/Query/Mysql/WeekOfYear.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace DoctrineExtensions\Query\Mysql;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+
+class WeekOfYear extends FunctionNode
+{
+    private $date;
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return 'WEEKOFYEAR(' . $sqlWalker->walkArithmeticPrimary($this->date) . ')';
+    }
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->date = $parser->ArithmeticPrimary();
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
