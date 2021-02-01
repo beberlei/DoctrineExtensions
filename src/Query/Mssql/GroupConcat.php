16ff7307b32c5fb7a8f1e24fbdd2fd851f8fd0f3
Andrei Luzhanski <andrei.luzhanski@tmx.com.ua>
2021-02-01 18:06:05 +0300

    Add MSSQL queries

---
+++ src/Query/Mssql/GroupConcat.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace DoctrineExtensions\Query\Mssql;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+
+class GroupConcat extends FunctionNode
+{
+    public $isDistinct = false;
+
+    public $pathExp = null;
+
+    public $separator = null;
+
+    public $orderBy = null;
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $lexer = $parser->getLexer();
+        if ($lexer->isNextToken(Lexer::T_DISTINCT)) {
+            $parser->match(Lexer::T_DISTINCT);
+
+            $this->isDistinct = true;
+        }
+
+        // first Path Expression is mandatory
+        $this->pathExp = [];
+        if ($lexer->isNextToken(Lexer::T_IDENTIFIER)) {
+            $this->pathExp[] = $parser->StringExpression();
+        } else {
+            $this->pathExp[] = $parser->SingleValuedPathExpression();
+        }
+
+        while ($lexer->isNextToken(Lexer::T_COMMA)) {
+            $parser->match(Lexer::T_COMMA);
+            $this->pathExp[] = $parser->StringPrimary();
+        }
+
+        if ($lexer->isNextToken(Lexer::T_ORDER)) {
+            $this->orderBy = $parser->OrderByClause();
+        }
+
+        if ($lexer->isNextToken(Lexer::T_IDENTIFIER)) {
+            if (strtolower($lexer->lookahead['value']) !== 'separator') {
+                $parser->syntaxError('separator');
+            }
+            $parser->match(Lexer::T_IDENTIFIER);
+
+            $this->separator = $parser->StringPrimary();
+        }
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        $fields = [];
+        foreach ($this->pathExp as $pathExp) {
+            $fields[] = $pathExp->dispatch($sqlWalker);
+        }
+
+        $field = sprintf('%s', implode(', ', $fields));
+
+        $separator = $this->separator ? $sqlWalker->walkStringPrimary($this->separator) : ', ';
+
+        $fieldsort = $this->orderBy ? "WITHIN GROUP (ORDER BY {$sqlWalker->walkOrderByClause($this->orderBy)})" : '';
+
+        return "STRING_AGG({$field}, '{$separator}') {$fieldsort}";
+    }
+}
