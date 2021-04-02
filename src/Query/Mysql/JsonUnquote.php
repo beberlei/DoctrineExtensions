43ca664a92b0807d98f8a13ad689bf9a788d22a7
fd6130 <fd6130@git.com>
2021-04-02 15:34:53 +0800

    add MySQL JSON_UNQUOTE()

---
+++ src/Query/Mysql/JsonUnquote.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace DoctrineExtensions\Query\Mysql;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+class JsonUnquote extends FunctionNode
+{
+    protected $value;
+
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->value = $parser->StringPrimary();
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        $value = $sqlWalker->walkStringPrimary($this->value);
+
+        return sprintf('JSON_UNQUOTE(%s)', $value);
+    }
+}
---
+++ tests/Query/Mysql/JsonUnquoteTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace DoctrineExtensions\Tests\Query\Mysql;
+
+use DoctrineExtensions\Tests\Query\MysqlTestCase;
+
+class JsonUnquoteTest extends MysqlTestCase
+{
+    public function testJsonUnquoteWithValueAsParam(): void
+    {
+        $this->assertDqlProducesSql(
+            "SELECT JSON_UNQUOTE(:value) from DoctrineExtensions\Tests\Entities\Blank as blank",
+            "SELECT JSON_UNQUOTE(?) AS sclr_0 FROM Blank b0_",
+            ['value' => '"1"']
+        );
+    }
+
+    public function testJsonUnquoteWithValueAsNoParam(): void
+    {
+        $this->assertDqlProducesSql(
+            "SELECT JSON_UNQUOTE(:value) from DoctrineExtensions\Tests\Entities\Blank as blank",
+            "SELECT JSON_UNQUOTE(?) AS sclr_0 FROM Blank b0_"
+        );
+    }
+
+    public function testJsonUnquoteWithValueAsExplicit(): void
+    {
+        $this->assertDqlProducesSql(
+            "SELECT JSON_UNQUOTE('\"1\"') from DoctrineExtensions\Tests\Entities\Blank as blank",
+            "SELECT JSON_UNQUOTE('\"1\"') AS sclr_0 FROM Blank b0_"
+        );
+    }
+}
