dbdcc6d3021757d18ed2855c6c198ef5248cc48f
fd6130 <fd6130@git.com>
2021-04-03 15:25:31 +0800

    update JSON_EXTRACT test case

--- tests/Query/Mysql/JsonExtractTest.php
+++ tests/Query/Mysql/JsonExtractTest.php
@@ -9,8 +9,8 @@ class JsonExtractTest extends MysqlTestCase
     public function testJsonExtractWithPathAsParam(): void
     {
         $this->assertDqlProducesSql(
-            "SELECT JSON_CONTAINS('{}', :path) from DoctrineExtensions\Tests\Entities\Blank as blank",
-            "SELECT JSON_CONTAINS('{}', ?) AS sclr_0 FROM Blank b0_",
+            "SELECT JSON_EXTRACT('{}', :path) from DoctrineExtensions\Tests\Entities\Blank as blank",
+            "SELECT JSON_EXTRACT('{}', ?) AS sclr_0 FROM Blank b0_",
             ['path' => '$[0]']
         );
     }
@@ -18,16 +18,16 @@ class JsonExtractTest extends MysqlTestCase
     public function testJsonExtractWithPathAsNoParam(): void
     {
         $this->assertDqlProducesSql(
-            "SELECT JSON_CONTAINS('{}', :path) from DoctrineExtensions\Tests\Entities\Blank as blank",
-            "SELECT JSON_CONTAINS('{}', ?) AS sclr_0 FROM Blank b0_"
+            "SELECT JSON_EXTRACT('{}', :path) from DoctrineExtensions\Tests\Entities\Blank as blank",
+            "SELECT JSON_EXTRACT('{}', ?) AS sclr_0 FROM Blank b0_"
         );
     }
 
     public function testJsonExtractWithPathAsExplicit(): void
     {
         $this->assertDqlProducesSql(
-            "SELECT JSON_CONTAINS('{}', '$[0]') from DoctrineExtensions\Tests\Entities\Blank as blank",
-            "SELECT JSON_CONTAINS('{}', '$[0]') AS sclr_0 FROM Blank b0_"
+            "SELECT JSON_EXTRACT('{}', '$[0]') from DoctrineExtensions\Tests\Entities\Blank as blank",
+            "SELECT JSON_EXTRACT('{}', '$[0]') AS sclr_0 FROM Blank b0_"
         );
     }
 }
