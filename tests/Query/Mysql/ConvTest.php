fb8f16494a1e71f321f2fcf8855a07aaf6db697b
shyam <shyam@ghanshyamdigital.com>
2021-06-01 22:57:47 +0530

    fix test case.

--- tests/Query/Mysql/ConvTest.php
+++ tests/Query/Mysql/ConvTest.php
@@ -9,8 +9,8 @@ class ConvTest extends MysqlTestCase
     public function testReplace()
     {
         $this->assertDqlProducesSql(
-            "SELECT CONV(110, 2, 10) from DoctrineExtensions\Tests\Entities\Blank b",
-            'SELECT CONV(110, 2, 10) AS sclr_0 FROM Blank b0_'
+            "SELECT CONV('110', 2, 10) from DoctrineExtensions\Tests\Entities\Blank b",
+            "SELECT CONV('110', 2, 10) AS sclr_0 FROM Blank b0_"
         );
     }
 }
