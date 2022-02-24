b5d6a3164b39773bf71ce8cded78f19319a49467
Maikel Ortega Hernandez <maikeloh@gmail.com>
2022-02-24 14:10:04 -0500

    - updated composer.json

--- src/Query/MysqlWalker.php
+++ src/Query/MysqlWalker.php
@@ -11,7 +11,7 @@ class MysqlWalker extends SqlWalker
      *
      * @return array|string
      */
-    public function walkSelectClause($selectClause) : mixed
+    public function walkSelectClause($selectClause)
     {
         $sql = parent::walkSelectClause($selectClause);
 
