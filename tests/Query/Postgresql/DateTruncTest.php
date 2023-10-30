57046152e69b3c4ff4f4184f87d0883aade90c1b
Florent Destremau <florent@windoo.fr>
2023-10-30 18:38:58 +0100

    Added test case

--- tests/Query/Postgresql/DateTruncTest.php
+++ tests/Query/Postgresql/DateTruncTest.php
@@ -3,6 +3,7 @@
 namespace DoctrineExtensions\Tests\Query\Postgresql;
 
 use Doctrine\ORM\QueryBuilder;
+use DoctrineExtensions\Tests\Entities\Date;
 use DoctrineExtensions\Tests\Query\PostgresqlTestCase;
 
 class DateTruncTest extends PostgresqlTestCase
@@ -14,8 +15,21 @@ class DateTruncTest extends PostgresqlTestCase
             ->select("date_trunc('YEAR', dt.created)")
             ->from('DoctrineExtensions\Tests\Entities\Date', 'dt');
 
-        $expected = "SELECT DATE_TRUNC('YEAR', d0_.created) AS sclr_0 FROM Date d0_";
+        $expected = "SELECT DATE_TRUNC('YEAR'::text, d0_.created::timestamp) AS sclr_0 FROM Date d0_";
 
         $this->assertEquals($expected, $queryBuilder->getQuery()->getSQL());
+    }
+
+    public function testDateTruncCondition()
+    {
+        $queryBuilder = $this->entityManager->getRepository(Date::class)
+            ->createQueryBuilder('dt')
+            ->where("date_trunc('YEAR', dt.created) = date_trunc('YEAR', :date)")
+            ->setParameter('date', new \DateTime('2010-01-01'));
+
+        $expected = "SELECT d0_.id AS id_0, d0_.created AS created_1 FROM Date d0_ WHERE DATE_TRUNC('YEAR'::text, d0_.created::timestamp) = DATE_TRUNC('YEAR'::text, ?::timestamp)";
+
+        $this->assertEquals($expected, $queryBuilder->getQuery()->getSQL());
+
     }
 }
