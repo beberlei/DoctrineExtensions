4aa592bc37d84ab62b073149fc1bdac8c05346e8
Karoly Gossler <connor@connor.hu>
2024-01-28 02:58:23 +0100

    Code Sniffer: manual fix Squiz.Strings.DoubleQuoteUsage.ContainsVar

--- tests/Query/Mysql/CastTest.php
+++ tests/Query/Mysql/CastTest.php
@@ -15,11 +15,11 @@ class CastTest extends MysqlTestCase
     public function testSimpleSqlQuery(): void
     {
         $query = $this->entityManager->createQuery(
-            "SELECT CAST(blog.latitude AS SIGNED) FROM {$this->entityLong} blog"
+            'SELECT CAST(blog.latitude AS SIGNED) FROM ' . $this->entityLong . ' blog'
         );
 
         static::assertEquals(
-            "SELECT CAST(b0_.latitude AS SIGNED) AS sclr_0 FROM {$this->entityShort} b0_",
+            'SELECT CAST(b0_.latitude AS SIGNED) AS sclr_0 FROM ' . $this->entityShort . ' b0_',
             $query->getSQL()
         );
     }
@@ -27,11 +27,11 @@ class CastTest extends MysqlTestCase
     public function testMultipleCastWithFieldAliasSqlQuery(): void
     {
         $query = $this->entityManager->createQuery(
-            "SELECT CAST(blog.latitude AS UNSIGNED), CAST(blog.latitude AS SIGNED) FROM {$this->entityLong} blog"
+            'SELECT CAST(blog.latitude AS UNSIGNED), CAST(blog.latitude AS SIGNED) FROM ' . $this->entityLong . ' blog'
         );
 
         static::assertEquals(
-            "SELECT CAST(b0_.latitude AS UNSIGNED) AS sclr_0, CAST(b0_.latitude AS SIGNED) AS sclr_1 FROM {$this->entityShort} b0_",
+            'SELECT CAST(b0_.latitude AS UNSIGNED) AS sclr_0, CAST(b0_.latitude AS SIGNED) AS sclr_1 FROM ' . $this->entityShort . ' b0_',
             $query->getSQL()
         );
     }
@@ -39,11 +39,11 @@ class CastTest extends MysqlTestCase
     public function testWithParametersSqlQuery(): void
     {
         $query = $this->entityManager->createQuery(
-            "SELECT CAST(blog.latitude AS DECIMAL(10, 2)) FROM {$this->entityLong} blog"
+            'SELECT CAST(blog.latitude AS DECIMAL(10, 2)) FROM ' . $this->entityLong . ' blog'
         );
 
         static::assertEquals(
-            "SELECT CAST(b0_.latitude AS DECIMAL(10, 2)) AS sclr_0 FROM {$this->entityShort} b0_",
+            'SELECT CAST(b0_.latitude AS DECIMAL(10, 2)) AS sclr_0 FROM ' . $this->entityShort . ' b0_',
             $query->getSQL()
         );
     }
