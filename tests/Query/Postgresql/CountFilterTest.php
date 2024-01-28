2ff2d11708929e2430bdfd7b6e5031594bb6a5b9
Karoly Gossler <connor@connor.hu>
2024-01-28 02:59:35 +0100

    Code Sniffer: apply rule Generic.Formatting.MultipleStatementAlignment.NotSame

--- tests/Query/Postgresql/CountFilterTest.php
+++ tests/Query/Postgresql/CountFilterTest.php
@@ -11,7 +11,7 @@ class CountFilterTest extends PostgresqlTestCase
     public function testCountFilter(): void
     {
         $dql = 'SELECT COUNT_FILTER(b, WHERE b.id = :id) FROM DoctrineExtensions\Tests\Entities\Blank b';
-        $q = $this->entityManager->createQuery($dql);
+        $q   = $this->entityManager->createQuery($dql);
         $q->setParameter('id', 1);
         $sql = 'SELECT COUNT(b0_.id) FILTER( WHERE b0_.id = ?) AS sclr_0 FROM Blank b0_';
         $this->assertEquals($sql, $q->getSql());
