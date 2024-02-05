70e77ad48be22666c823caf76558c273ad332d71
Karoly Gossler <connor@connor.hu>
2024-02-05 14:25:19 +0100

    Add TestCase parent to Atan2Test class

--- tests/Query/Mysql/Atan2Test.php
+++ tests/Query/Mysql/Atan2Test.php
@@ -2,13 +2,15 @@
 
 namespace DoctrineExtensions\Tests\Query\Mysql;
 
-class Atan2Test
+use DoctrineExtensions\Tests\Query\MysqlTestCase;
+
+class Atan2Test extends MysqlTestCase
 {
     public function testAtan2(): void
     {
         $this->assertDqlProducesSql(
-            'SELECT ATAN2(2) from DoctrineExtensions\Tests\Entities\Blank b',
-            'SELECT ATAN2(2) AS sclr_0 FROM Blank b0_'
+            'SELECT ATAN2(2, 1) from DoctrineExtensions\Tests\Entities\Blank b',
+            'SELECT ATAN2(2, 1) AS sclr_0 FROM Blank b0_'
         );
     }
 }
