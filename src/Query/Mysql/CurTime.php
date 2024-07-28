b4afd2ea9f8c29432f6b88adfd4b93ca0ef859b7
GiambaFr <45350810+GiambaFr@users.noreply.github.com>
2024-07-28 10:53:52 +0200

    Create CurTime.php

---
+++ src/Query/Mysql/CurTime.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace DoctrineExtensions\Query\Mysql;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\TokenType;
+
+/** @author JB */
+class CurTime extends FunctionNode
+{
+    public $time;
+
+    public function getSql(SqlWalker $sqlWalker): string
+    {
+        return 'CURTIME()';
+    }
+
+    public function parse(Parser $parser): void
+    {
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
+    }
+}
