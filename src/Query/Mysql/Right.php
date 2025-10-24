67b8768860f2abd16c3fe1c56abadfa8a9f89034
Cyril BIDAUD (Ext) <cyril.bidaud@capgemini.com>
2025-10-24 16:41:28 +0200

    feat: Add MySql string functions RIGHT and LEFT

---
+++ src/Query/Mysql/Right.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace DoctrineExtensions\Query\Mysql;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\QueryException;
+use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\TokenType;
+
+/**
+ * @link https://dev.mysql.com/doc/refman/en/string-functions.html#function_right
+ *
+ * @author Cyril 'Yepzy' B <me@yepzy.dev>
+ */
+class Right extends FunctionNode
+{
+    private $field;
+    private $length;
+
+    /**
+     * @throws QueryException
+     */
+    public function parse(Parser $parser): void
+    {
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
+        $this->field = $parser->StringPrimary();
+        $parser->match(TokenType::T_COMMA);
+        $this->length = $parser->ArithmeticPrimary();
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker): string
+    {
+        return 'RIGHT(' .
+            $this->field->dispatch($sqlWalker) . ', ' .
+            $this->length->dispatch($sqlWalker) .
+        ')';
+    }
+}
